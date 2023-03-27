a66e6c9e4e2dc1e4dd8bfcb7c2f64530426046b0
Will Smillie <will@smillie.me>
2023-03-27 16:40:59 -0400

    Donate button

--- src/tools/Donate.js
+++ src/tools/Donate.js
@@ -9,10 +9,10 @@ const Content = () => (
     <Typography gutterBottom>
       If you clicked here, thank you for even considering it! If you find NFT Tool Kit useful, and want to support
       further development (or help me and my dog eat) consider leaving a tip.
+      <BuyMeACoffee sx={{ minWidth: '100px' }} />
     </Typography>
 
     <Stack direction="row" spacing={2}>
-      <BuyMeACoffee sx={{ minWidth: '100px' }} />
       <Button variant="contained" large>
         Fenneckit.eth
       </Button>
