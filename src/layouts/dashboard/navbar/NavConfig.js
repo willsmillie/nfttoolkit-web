47aa2b332fd457a6707e67213cb3d41fef18c0ea
Will Smillie <will@smillie.me>
2023-03-27 17:56:35 -0400

    add github link

--- src/layouts/dashboard/navbar/NavConfig.js
+++ src/layouts/dashboard/navbar/NavConfig.js
@@ -1,4 +1,5 @@
 // components
+import { GitHub } from '@mui/icons-material';
 import SvgIconStyle from '../../../components/SvgIconStyle';
 
 // ----------------------------------------------------------------------
@@ -7,6 +8,7 @@ const getIcon = (name) => <SvgIconStyle src={`/assets/icons/navbar/${name}.svg`}
 
 const ICONS = {
   user: getIcon('ic_user'),
+  github: <GitHub />,
   ecommerce: getIcon('ic_ecommerce'),
   analytics: getIcon('ic_analytics'),
   dashboard: getIcon('ic_dashboard'),
@@ -17,7 +19,10 @@ const navConfig = [
   // ----------------------------------------------------------------------
   {
     subheader: 'NFT Tool Kit',
-    items: [{ title: 'Tools', path: '/tools', icon: ICONS.dashboard }],
+    items: [
+      { title: 'Tools', path: '/tools', icon: ICONS.dashboard },
+      { title: 'Github', path: 'http://github.com/willsmillie/nfttoolkit-web', icon: ICONS.github },
+    ],
   },
 
   // MANAGEMENT
