b7100448d4fd6e901aaa9526b3231dcca5e48d30
Will Smillie <will@smillie.me>
2023-03-27 12:50:57 -0400

    code cleanup

--- src/hooks/useDebounce.js
+++ src/hooks/useDebounce.js
@@ -1,6 +1,7 @@
 import { useEffect, useCallback } from 'react';
 
 export default function useDebounce(effect, dependencies, delay) {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   const callback = useCallback(effect, dependencies);
 
   useEffect(() => {
