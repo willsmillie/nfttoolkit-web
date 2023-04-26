141a2c1448542093af7369227fe74caa061b3669
willsmillie <williamfsmillie@gmail.com>
2023-04-26 17:26:59 -0400

    fixed connect button making multiple wallet requests

--- src/components/ConnectButton.js
+++ src/components/ConnectButton.js
@@ -1,64 +1,21 @@
 /* eslint-disable react/jsx-no-undef */
 /* eslint-disable react/jsx-key */
-import React from 'react';
+import React, { useContext } from 'react';
 import { Typography, Button, Menu, MenuItem, ListItemIcon, ListItemText } from '@mui/material';
 
 // @web3-react
 import { useWeb3React } from '@web3-react/core';
-import { InjectedConnector } from '@web3-react/injected-connector';
-import { isAdmin } from '../utils/web3';
+import { AuthContext } from '../contexts/L2Context';
 
 export default function ConnectPopover() {
-  const { activate, deactivate, active, account } = useWeb3React();
+  const { connect, disconnect } = useContext(AuthContext);
+  const { active, account } = useWeb3React();
+
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
 
-  const Injected = new InjectedConnector({
-    supportedChainIds: [1, 3, 4, 5, 42],
-  });
-
   const handleClick = (event) => setAnchorEl(event.currentTarget);
   const handleClose = () => setAnchorEl(null);
-
-  const MenuItems = () => {
-    let results = [];
-
-    if (active && account) {
-      results = [
-        // <MenuItem onClick={metaMask}>
-        //   <ListItemIcon>
-        //     <img src="https://static.loopring.io/assets/svg/meta-mask.svg" alt="MetaMask" height="36" />
-        //   </ListItemIcon>
-        //   <ListItemText>Metamask</ListItemText>
-        // </MenuItem>,
-        // <MenuItem onClick={walletConnect}>
-        //   <ListItemIcon>
-        //     <img src="https://static.loopring.io/assets/svg/wallet-connect.svg" alt="WalletConnect" height="36" />
-        //   </ListItemIcon>
-        //   <ListItemText>WalletConnect</ListItemText>
-        // </MenuItem>,
-        //     <ListItemIcon>
-        //       <img src="https://static.loopring.io/assets/svg/coinbase-wallet.svg" alt="Coinbase" height="36" />
-        //     </ListItemIcon>
-        //     <ListItemText>Coinbase</ListItemText>
-        //   </MenuItem>,
-        <MenuItem onClick={() => deactivate()}>Disconnect</MenuItem>,
-      ];
-
-      if (isAdmin(account)) results = [...results, <MenuItem onClick={handleClose}>Profile</MenuItem>];
-    } else {
-      results = [
-        <MenuItem onClick={() => activate(Injected)}>
-          <ListItemIcon>
-            <img src="https://static.loopring.io/assets/svg/gs.svg" alt="GameStop" height="36" />
-          </ListItemIcon>
-          <ListItemText>GameStop</ListItemText>
-        </MenuItem>,
-      ];
-    }
-
-    return results;
-  };
 
   return (
     <div>
@@ -94,7 +51,15 @@ export default function ConnectPopover() {
           'aria-labelledby': 'basic-button',
         }}
       >
-        <MenuItems />
+        {active && account && <MenuItem onClick={disconnect}>Disconnect</MenuItem>}
+        {!active && (
+          <MenuItem onClick={connect}>
+            <ListItemIcon>
+              <img src="https://static.loopring.io/assets/svg/gs.svg" alt="GameStop" height="36" />
+            </ListItemIcon>
+            <ListItemText>GameStop</ListItemText>
+          </MenuItem>
+        )}
       </Menu>
     </div>
   );
