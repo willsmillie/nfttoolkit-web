67a3c3c29f753acbde03cb509c25270c4501bfe8
Will Smillie <will@smillie.me>
2023-08-08 15:38:42 -0400

    analytics

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase/compat/app';
+import { getAnalytics, logEvent } from 'firebase/analytics';
 import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 
@@ -12,16 +13,23 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
 // Access Firestore instance
 const storage = firebase.storage();
 const db = firebase.firestore();
 
 const timestamp = firebase.firestore.FieldValue.serverTimestamp();
 
+// Initialize Analytics and get a reference to the service
+const analytics = getAnalytics(app);
+const logAnalytics = (name, data) => {
+  logEvent(analytics, name, data);
+  console.log(name, data);
+};
+
 // if (process.env.REACT_APP_DEVELOPMENT) {
 //   console.log('configuring emus');
 //   db.useEmulator('127.0.0.1', 8080);
 // }
 
-export { db, storage, timestamp };
+export { db, storage, timestamp, logAnalytics };
